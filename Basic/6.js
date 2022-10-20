764de3ed926029fcbb2912752ed56384dcf0ab7e
Wilbert-Tosoy <brownabelard@gmail.com>
2022-10-20 21:03:03 +0800

    Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar.

--- Basic/6.js
+++ Basic/6.js
@@ -1,8 +1,9 @@
 // Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar.
 
-const isLeapYear = () => {
-    let year = 2022;
+let year = 2024;
+
+const isLeapYear = (year) => {
     return (year % 4 === 0) ? "Leap Year" : "Not a Leap Year";
 }
 
-console.log(isLeapYear());
+console.log(isLeapYear(year));
