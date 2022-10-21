9807459a535564b8c74564282ac77bd25c2cd633
Wilbert-Tosoy <brownabelard@gmail.com>
2022-10-21 21:12:27 +0800

    Write a JavaScript program to compute the sum of the two given integers. If the two values are same, then returns triple their sum.

---
+++ Basic/16.js
@@ -0,0 +1,10 @@
+// Write a JavaScript program to compute the sum of the two given integers. If the two values are same, then returns triple their sum.
+
+const x = 13, y = 13;
+const sum = x + y;
+
+if (x === y) {
+    console.log(Math.pow(sum, 3));
+} else {
+    console.log(sum)
+}
