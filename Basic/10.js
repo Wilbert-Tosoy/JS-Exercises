63609080f2e9993edf43f03391c37a4280458000
Wilbert-Tosoy <brownabelard@gmail.com>
2022-10-21 10:07:43 +0800

    Write a JavaScript program to calculate multiplication and division of two numbers.

---
+++ Basic/10.js
@@ -0,0 +1,7 @@
+// Write a JavaScript program to calculate multiplication and division of two numbers.
+
+let firstNumber = 10;
+let secondNumber = 12;
+
+console.log(firstNumber * secondNumber);
+console.log(firstNumber / secondNumber);
